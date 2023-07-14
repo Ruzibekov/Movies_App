1829dba0a6696e327f7d61841b51a81b15718f29
ruzibekov <ruzibekov44@gmail.com>
2023-07-14 12:43:37 +0500

    add: navigation graph

---
+++ app/src/main/java/com/example/moviesapp/ui/screens/details/DetailsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.moviesapp.ui.screens.details
+
+import androidx.fragment.app.Fragment
+import com.example.moviesapp.R
+
+class DetailsFragment : Fragment(R.layout.fragment_details) {
+}
---
+++ data/src/main/java/com/example/data/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.data
+
+object Constants {
+
+    const val API_KEY = "1a2222f601ba78988144247defc81cbf"
+    const val BASE_URL = "https://api.themoviedb.org/"
+}
